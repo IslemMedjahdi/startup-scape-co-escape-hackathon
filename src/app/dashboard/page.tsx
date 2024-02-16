c3b520481f6769aef5e00edc5c3e2dc773d0a51f
IslemMedjahdi <ki_medjahdi@esi.dz>
2024-02-16 11:39:00 +0100

    Add new components and update dependencies

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "CSE Dashboard | Home",
+};
+
+const DashboardHomePage = async () => {
+  return <div>Dashboard Home Page</div>;
+};
+
+export default DashboardHomePage;
