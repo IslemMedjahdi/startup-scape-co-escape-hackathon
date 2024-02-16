ad6f824485ff1ac80c47b23af562bfeb820ebdb8
IslemMedjahdi <ki_medjahdi@esi.dz>
2024-02-16 16:45:38 +0100

    Add additional fields to RegisterRequest interface

--- src/app/api/auth/register/request.ts
+++ src/app/api/auth/register/request.ts
@@ -1,4 +1,37 @@
 export interface RegisterRequest {
     email: string;
     password: string;
+    founderFirstName: string;
+    founderLastName: string;
+    phoneNumber: string;
+    companyName: string;
+    industry: string;
+    companyPhoneNumber: string;
+    companyEmail: string;
+    companyWebsite?: string;
+    socialMedia: {
+        facebook?: string;
+        linkedin?: string;
+        twitter?: string;
+        instagram?: string;
+    };
+
+    companyDescription: string;
+    companyAddress: {
+        street: string;
+        city: string;
+        state: string;
+        zip: string;
+    };
+    yearFounded: number;
+
+    servicesDescription: string;
+    stageOfDevelopement: string;
+
+    targetMarket: string;
+
+    teamSizeFrom: number;
+    teamSizeTo: number;
+
+    goalsAndChallenges: string;
 }
